93ea39c563d6dc479d075769ab59596acbebea5d
sanjayhirani <42233284+sanjayhirani@users.noreply.github.com>
2025-09-29 22:59:12 +0100

    Update robinhood_sell_puts.py

--- robinhood_sell_puts.py
+++ robinhood_sell_puts.py
@@ -30,7 +30,7 @@ import numpy as np
 import pandas as pd
 
 # ------------------ CONFIG ------------------
-TICKERS = ["SNAP", "ACHR", "OPEN", "BBAI", "PTON", "ONDS", "GRAB", "LAC", "HTZ", "RZLV", "NVTS"]
+TICKERS = ["SNAP", "ACHR", "OPEN", "BBAI", "PTON", "ONDS", "GRAB", "LAC", "HTZ", "RZLV", "NVTS", "CLOV", "RIG, "LDI", "SPCE", "AMC", "LAZR"]
 NUM_EXPIRATIONS = 3
 NUM_PUTS = 3
 MIN_PRICE = 0.10
