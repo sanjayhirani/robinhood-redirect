a77305db4725ab27ed76075bf96b02395da79eaf
sanjayhirani <42233284+sanjayhirani@users.noreply.github.com>
2025-10-31 13:04:21 +0000

    Update robinhood_sell_puts.py

--- robinhood_sell_puts.py
+++ robinhood_sell_puts.py
@@ -52,9 +52,7 @@ def send_telegram_message(msg):
 
 # ------------------ LOGIN ------------------
 
-REFRESH_TOKEN = os.environ["RH_REFRESH_TOKEN"]
-r.login(username=USERNAME, password=PASSWORD, store_session=False, mfa_code=None, refresh_token=REFRESH_TOKEN)
-
+r.login(USERNAME, PASSWORD)
 today = datetime.now().date()
 cutoff = today + timedelta(days=config.get("expiry_limit_days", 30))
 
@@ -742,10 +740,3 @@ table_lines.append("</pre>")
 
 # Send Telegram alert
 send_telegram_message("\n".join(table_lines))
-
-
-
-
-
-
-
