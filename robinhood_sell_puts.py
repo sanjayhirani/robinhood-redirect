e48a4c09c670a4b76bc02ad05ff53d9c6dd65d59
sanjayhirani <42233284+sanjayhirani@users.noreply.github.com>
2025-11-02 20:27:44 +0000

    Update robinhood_sell_puts.py

--- robinhood_sell_puts.py
+++ robinhood_sell_puts.py
@@ -386,7 +386,7 @@ def scan_ticker(ticker_raw, ticker_clean):
                 if last_low == 0:
                     continue
                 dist_from_low = (strike_price - last_low) / last_low
-                if strike_price < last_low * 0.99:
+                if dist_from_low < -0.5:
                     continue
 
                 candidate_puts.append({
@@ -733,5 +733,6 @@ table_lines.append("</pre>")
 
 # Send Telegram alert
 send_telegram_message("\n".join(table_lines))
+
 
 
